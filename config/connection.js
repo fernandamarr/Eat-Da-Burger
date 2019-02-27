559e25e94575cd3a1948e6b5ef60d98759040c65
Fernanda Marroquin <fernanda.marroquin07@gmail.com>
2019-02-27 12:30:28 -0500

    add logic

--- config/connection.js
+++ config/connection.js
@@ -1,23 +1,32 @@
-// Set up MySQL connection.
+// Set up MySQL connection
+
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
+// set up connection
+var connection;
+
+if (process.env.JAWSDB_URL) {
+  // database is JawsDB on Heroku
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  // database is on localhost
+  connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: process.env.PASSWORD,
+    password: "",
     database: "burgers_db"
   });
-  
-  // Make connection.
-  connection.connect(function(err) {
-    if (err) {
-      console.error("error connecting: " + err.stack);
-      return;
-    }
-    console.log("connected as id " + connection.threadId);
-  });
-  
-  // Export connection for our ORM to use.
-  module.exports = connection;
-  
+}
+
+// Make connection.
+connection.connect(function (err) {
+  if (err) {
+    console.error("\nError connecting: " + err.stack + "\n");
+    return;
+  }
+  console.log("\nConnected as id " + connection.threadId + "\n");
+});
+
+// Export connection for our ORM to use.
+module.exports = connection;
